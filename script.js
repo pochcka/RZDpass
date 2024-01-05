b4617a53a7e53d6c28c0a4ee75491ba2cf982620
Гуменюк Z3100 <113640731+pochcka@users.noreply.github.com>
2024-01-05 04:16:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const barcode = document.getElementById("barcode");
 const overlay = document.getElementById("overlay");
 
-barcode.addEventListener('touchmove', (event) => {
+barcode.addEventListener('click', (event) => {
   overlay.style.display = "block"; // Показываем эффект
   overlay.style.left = event.offsetX + 'px';
   overlay.style.top = event.offsetY +60+ 'px';
@@ -12,6 +12,7 @@ barcode.addEventListener('touchmove', (event) => {
 barcode.addEventListener('touchend', (event) => {
   overlay.style.display = "none"; // Скрываем эффект
 });
+
 
 
 
